71feb50160dd4f98ced4fd25c80be046e459ce8a
prernabhandari2427 <prernabhandari2427@gmail.com>
2020-04-22 17:15:05 -0500

    test

--- selenium/src/section12/lecture94.java
+++ selenium/src/section12/lecture94.java
@@ -14,11 +14,7 @@ public class lecture94 {
 		System.setProperty("webdriver.chrome.driver", "/Users/prernabhandari/Downloads/chromedriver");
 		WebDriver driver = new ChromeDriver(options);
 		driver.get("https://www.path2usa.com/travel-companions");
-		System.out.println("Hello world");
-		System.out.println("Hello world");
-		System.out.println("Adding new code here");
-		System.out.println("Adding new code here 22");
-		System.out.println("Adding new code here 223");
+		
 		
 	}
 
