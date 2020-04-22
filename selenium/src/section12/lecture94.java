97693e43d0e3cac3c9334981a06029fdf955812f
prernabhandari2427 <prernabhandari2427@gmail.com>
2020-04-22 14:02:19 -0500

    Initialize project

---
+++ selenium/src/section12/lecture94.java
@@ -0,0 +1,22 @@
+package section12;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class lecture94 {
+
+	public static void main(String[] args) {
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		
+		System.setProperty("webdriver.chrome.driver", "/Users/prernabhandari/Downloads/chromedriver");
+		WebDriver driver = new ChromeDriver(options);
+		driver.get("https://www.path2usa.com/travel-companions");
+		
+		
+		
+	}
+
+}
