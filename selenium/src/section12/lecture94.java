78ad40507017fd39a41992239cf12895ccb172a1
prernabhandari2427 <prernabhandari2427@gmail.com>
2020-04-23 14:21:40 -0500

    added new test case

--- selenium/src/section12/lecture94.java
+++ selenium/src/section12/lecture94.java
@@ -1,12 +1,20 @@
 package section12;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+//import com.sun.tools.javac.util.List;
+
+
+
 public class lecture94 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--disable-notifications");
@@ -14,9 +22,20 @@ public class lecture94 {
 		System.setProperty("webdriver.chrome.driver", "/Users/prernabhandari/Downloads/chromedriver");
 		WebDriver driver = new ChromeDriver(options);
 		driver.get("https://www.path2usa.com/travel-companions");
-		
-		
+		driver.manage().window().maximize();
+		setparams sp = new setparams();
+		sp.setDate(driver);
+		sp.setfrom(driver);
+		sp.setTo(driver);
+		driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
+		if(driver.findElement(By.xpath("//div[@class='col-sm-12']//p")).getText().equals("No listing found")) {
+			System.out.println("no listing , test case pass");
+		}
+	
 	}
-
 }
- 
+	
+		
+		
+
+
---
+++ selenium/src/section12/setparams.java
@@ -0,0 +1,57 @@
+package section12;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class setparams {
+	
+
+
+
+public  void setDate(WebDriver driver) {
+	driver.findElement(By.id("travel_date")).click();
+	driver.findElement(By.cssSelector("th.datepicker-switch")).click();
+	driver.findElement(By.xpath("//div[@class='datepicker-months']//span[11]")).click();
+	driver.findElement(By.xpath("//div[@class='datepicker-days']//table/tbody/tr/td[@class='old day'][text()='30']")).click();
+	
+}
+
+public  void setfrom(WebDriver driver) {
+	
+	driver.findElement(By.id("travel_from")).click();
+	driver.findElement(By.id("travel_from")).sendKeys("A");
+	//System.out.println( "is displayed ::"+driver.findElement(By.xpath("//div[@id='ajax_listOfOptions']")).getText());
+	try {
+		Thread.sleep(1000L);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	WebElement peElement = driver.findElement(By.xpath("//div[@id='ajax_listOfOptions']"));
+	peElement.findElement(By.id("Abilene")).click();
+	
+}
+
+
+public  void setTo(WebDriver driver) {
+	driver.findElement(By.id("travel_to")).click();
+	driver.findElement(By.id("travel_to")).sendKeys("york");
+	try {
+		Thread.sleep(1000L);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
+	WebElement peElement = driver.findElement(By.xpath("//div[@id='ajax_listOfOptions']"));
+	driver.findElement(By.xpath("(//div[contains(@id,'New York')])[1]")).click();
+	
+	}
+}
+
+	
+	
